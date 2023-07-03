ea88ab6897cfde814c18485a76e70297e387e2d8
Byekibe <kibetpeter95@gmail.com>
2023-07-04 01:43:18 +0300

    Modify return value in books route and add packages to requirements.txt file

--- backend/server.py
+++ backend/server.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
-    return { "names": ["Peter", "Vincent", "Faith"]}
+@app.route("/books")
+def books():
+    return { "books": [{"name":"Travel", "author": "Peter"}, {"name": "Minimalist", "author": "Vincent"}, {"name": "Ghosts", "author": "Faith"}]}
 
 @app.route("/about")
 def about():
